f38a052d2e6e4728185c02314a95eeb0164acff3
geza-m <110627477+geza-m@users.noreply.github.com>
2023-01-19 18:24:17 +0100

    code completed as per excercise.

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,17 @@
-const caesar = function() {
+const caesar = function(s = "", n) {
 
+    let cypher = "";
+    for (let i=0; i < s.length; i++) {
+        let ascii = s.charCodeAt(i);
+        cond = (ascii >= 65 && ascii <= 90) ||  (ascii >= 97 && ascii <= 122);
+        cypher += String.fromCharCode(cond ? ascii +n: ascii);  
+    }
+    return cypher;
 };
+
+console.log(caesar("ab cd",2));
+
+
 
 // Do not edit below this line
 module.exports = caesar;
